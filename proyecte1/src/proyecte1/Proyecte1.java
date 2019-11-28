1cf23bf9393e04cae51a8df8caf2f6a353527b8c
Dani Marin <dmarin2000@insgabrielamistral.cat>
2019-11-28 19:06:46 +0100

    Primera parte (pedidad de datos)

--- proyecte1/src/proyecte1/Proyecte1.java
+++ proyecte1/src/proyecte1/Proyecte1.java
@@ -1,5 +1,7 @@
 
 package proyecte1;
+import java.util.Scanner;
+
 /**
  *
  * @author Dani
@@ -8,7 +10,37 @@ public class Proyecte1 {
 
   
     public static void main(String[] args) {
+       //metodo para meter en un array los dos valores para calcular el imc
+       double peso = IntroducirPeso();
+       //System.out.println(peso);
        
+       double altura = IntroducirAltura();
+       //System.out.println(altura);
+        
+        //metodo para calcular el IMC
+        calcularIMC(peso, altura);
+        
     }
     
+    public static double IntroducirPeso(){
+        Scanner sc = new Scanner(System.in);   
+        System.out.println("Introduce tu peso: ");
+        String p = sc.nextLine();
+        double peso = Double.parseDouble(p);
+ 
+    return peso;
+    }
+    
+    public static double IntroducirAltura(){
+     Scanner sc = new Scanner(System.in);   
+        System.out.println("Introduce tu altura: ");
+        String p = sc.nextLine();
+        double altura = Double.parseDouble(p);
+ 
+    return altura;
+    }
+    
+    public static void calcularIMC(double peso, double altura){
+        //formula para calcular el IMC
+    }
 }
